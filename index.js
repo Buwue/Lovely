398c93b2afd284b89a960ed7272cfad77ed2dce8
Buwue <88902780+Buwue@users.noreply.github.com>
2025-03-06 00:16:49 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+$(function() {
+    function randomNum(m, n) {
+      m = parseInt(m);
+      n = parseInt(n);
+      return Math.floor(Math.random() * (n - m + 1)) + m;
+    }
+    
+    function heartAnimation() {
+      $this = $('.effect-text');
+      var heartCount = ($this.width()/100)*20;
+      for (var i = 0; i< heartCount; i++) {
+        var heartSize = (randomNum(60, 120) / 10);
+        $this.append('<span class="tiny-heart" style="top: '+ randomNum(0, 300) +'%; left: '+ randomNum(0, 100) +'%; width: '+ heartSize +'px; height: '+ heartSize +'px ; animation-delay: -'+ randomNum(0, 3) +'s; animation-duration: '+ randomNum(2, 5) +'s"></span>')
+      }
+    }
+    
+    heartAnimation();
+  })
